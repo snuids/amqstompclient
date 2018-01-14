72a2ca5174149cc53a2d9eb9b6d679ca1e879430
snuids <a.marchand@progeri.com>
2018-01-14 18:28:34 +0100

    Save point

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'amqstompclient',
+  packages = ['amqstompclient'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Active MQ Python Client Using Stomp',
+  author = 'snuids',
+  author_email = '',
+  url = 'https://github.com/snuids/amqstompclient', # use the URL to the github repo
+  download_url = 'https://github.com/snuids/amqstompclient/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['ActiveMQ', 'stomp', 'client'], # arbitrary keywords
+  classifiers = [],
+)i
