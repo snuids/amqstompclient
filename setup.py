27927b2b0f03164ce419cb4acbf93def8b543c48
Arnaud Marchand <amarchand@icloud.com>
2023-02-22 14:00:40 +0100

    Restored setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'amqstompclient',
+  packages = ['amqstompclient'], # this must be the same as the name above
+  version = '1.2.0',
+  description = 'Active MQ Python Client Using Stomp',
+  author = 'snuids',
+  author_email = 'snuids@mannekentech.com',
+  url = 'https://github.com/snuids/amqstompclient', 
+  download_url = 'https://github.com/snuids/amqstompclient/archive/1.2.0.tar.gz',
+  keywords = ['ActiveMQ', 'stomp', 'client'], # arbitrary keywords
+  classifiers = [],
+)
