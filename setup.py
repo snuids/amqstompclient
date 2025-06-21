f0909fafe23154c8854d44b0544b3183c103ef35
Arnaud Marchand <amarchand@icloud.com>
2025-06-21 13:02:21 +0200

    v2.0.3

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'amqstompclient',
   packages = ['amqstompclient'], # this must be the same as the name above
-  version = '2.0.1',
+  version = '2.0.2',
   description = 'Active MQ Python Client Using Stomp',
   author = 'snuids',
   author_email = 'snuids@mannekentech.com',
   url = 'https://github.com/snuids/amqstompclient', 
-  download_url = 'https://github.com/snuids/amqstompclient/archive/2.0.1.tar.gz',
+  download_url = 'https://github.com/snuids/amqstompclient/archive/2.0.2.tar.gz',
   keywords = ['ActiveMQ', 'stomp', 'client'], # arbitrary keywords
   classifiers = [],
 )
